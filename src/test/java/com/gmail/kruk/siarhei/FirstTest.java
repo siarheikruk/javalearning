b72b33b3b0420377461cd42f5ff3f0f072a6cdf0
Siarhei Kruk <siarhei.kruk@gmail.com>
2018-05-09 19:39:33 +0300

    Final first test commit

--- src/test/java/com/gmail/kruk/siarhei/FirstTest.java
+++ src/test/java/com/gmail/kruk/siarhei/FirstTest.java
@@ -1,4 +1,47 @@
 package com.gmail.kruk.siarhei;
 
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class FirstTest {
+
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/home/user/Work/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://mail.ukr.net/desktop/login");
+    }
+    @Test
+    public void userLogin() {
+        WebElement loginField = driver.findElement(By.id("login"));
+        loginField.sendKeys("autotestorgua");
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("testpass");
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Войти']"));
+        loginButton.click();
+        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
+        String mailUser = profileUser.getText();
+        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
+    }
+    @AfterClass
+    public static void tearDown() {
+        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
+        menuUser.click();
+        WebElement logoutButton = driver.findElement(By.id("login__logout"));
+        logoutButton.click();
+        driver.quit();
+    }
 }
