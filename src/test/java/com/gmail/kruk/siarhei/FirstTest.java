6b687101eb319c64a41f53dce3d9d6f317eaca4a
Siarhei Kruk <siarhei.kruk@gmail.com>
2018-05-13 21:03:16 +0300

    Test refactoring
    Changes foe another webapp. URLs are deleted till next changes.

--- src/test/java/com/gmail/kruk/siarhei/FirstTest.java
+++ src/test/java/com/gmail/kruk/siarhei/FirstTest.java
@@ -14,34 +14,40 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
-    private static WebDriver driver;
+    private static WebDriver driver = new ChromeDriver();
 
     @BeforeClass
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "/home/user/Work/chromedriver.exe");
-        driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
+        driver.get("");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://mail.ukr.net/desktop/login");
     }
     @Test
-    public void userLogin() {
-        WebElement loginField = driver.findElement(By.id("id-1"));
-        loginField.sendKeys("autotestorgua");
-        WebElement passwordField = driver.findElement(By.id("id-2"));
-        passwordField.sendKeys("testpass");
-        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Войти']"));
-        loginButton.click();
-        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
-        String mailUser = profileUser.getText();
-        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
+    public void page1() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement button1 = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/form/question-group-content[1]/div/div/question-content/div/button[2]"));
+        button1.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement field1 = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/form/question-group-content[3]/div/div/question-content[2]/div/div/div[2]/input"));
+        field1.sendKeys("6");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement button2 = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/form/question-group-content[3]/div/div/question-content[3]/div/button[1]"));
+        button2.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement button3 = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/form/question-group-content[6]/div/div/question-content/div/label[1]/input"));
+        button3.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement button4 = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/form/question-group-content[8]/div/div/question-content/div/label/input"));
+        button4.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement button5 = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/form/div[2]/button"));
+        button5.click();
+
     }
     @AfterClass
     public static void tearDown() {
-        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
-        menuUser.click();
-        WebElement logoutButton = driver.findElement(By.id("login__logout"));
-        logoutButton.click();
         driver.quit();
     }
 }
